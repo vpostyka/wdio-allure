303616c50464fe605d47df295f349825b3952789
Vladimir <99782728+vpostyka@users.noreply.github.com>
2022-09-12 22:29:17 -0700

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -135,7 +135,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'//,['allure', {outputDir: './allure-results'}]
+    reporters: ['spec',['allure', {outputDir: './allure-results'}]
          ],
     //
     // Options to be passed to Mocha.
